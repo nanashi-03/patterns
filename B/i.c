fcaaa8d1fe15e868fc63fb226823b3e1996f32d0
Nanashi <40239873+nanashi-03@users.noreply.github.com>
2022-02-11 04:01:08 +0530

    Add files via upload

---
+++ B/i.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int main()
+{
+	int i, j, n, a=0;
+	printf("Enter a number:");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		for(j=1; j<=n-i+1; j++)
+		{
+			a++;
+			if(a%2==0)
+			{
+				printf("1");
+			}
+			else
+			{
+				printf("0");
+			}
+		}
+		printf("\n");
+	}
+	for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            a++;
+			if(a%2==0)
+			{
+				printf("1");
+			}
+			else
+			{
+				printf("0");
+			}
+        }
+        printf("\n");
+    }
+	return 0;
+}
---
+++ B/l.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+	int i, j, n, a=0;
+	printf("Enter a number:");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		for(j=1; j<=n-i+1; j++)
+		{
+			a=(i+j)*(i+j);
+			printf("%5d",a);
+		}
+		printf("\n");
+	}
+	for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            a=((n-i+1)+j)*((n-i+1)+j);
+			printf("%5d",a);
+        }
+        printf("\n");
+    }
+	return 0;
+}

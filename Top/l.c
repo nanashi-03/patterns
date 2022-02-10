a9d07ee819716b08b68f7601db64881552adb640
Nanashi <40239873+nanashi-03@users.noreply.github.com>
2022-02-10 19:11:10 +0530

    Add files via upload

---
+++ Top/l.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+	int i, j, n, k, a=0;
+	printf("Enter a number:");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		for(k=1;k<i;k++)
+		{
+			printf("   ");
+		}
+		for(j=1; j<=n-i+1; j++)
+		{
+			a=(i+j)*(i+j);
+			printf("%3d",a);
+		}
+		for(j=n-i+1;j>=1;j--)
+		{
+			a=(i+j)*(i+j);
+			printf("%3d",a);
+		}
+		printf("\n");
+	}
+	return 0;
+}

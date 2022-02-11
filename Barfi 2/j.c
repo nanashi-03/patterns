36ab6cff4da97964e73612d2da1ae60f630afe70
Nanashi <40239873+nanashi-03@users.noreply.github.com>
2022-02-11 05:43:48 +0530

    Add files via upload

---
+++ Barfi 2/j.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+	int i, j, n, k, a=0;
+	printf("Enter a number:");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		
+		for(k=1;k<n-i+1;k++)
+		{
+			printf("   ");
+		}
+		a=i;
+		for(j=1; j<=i; j++)
+		{
+			printf("%3d", a);
+			a++;
+		}
+		printf("\n");
+	}
+	for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n-i+1;j++)
+        {
+            printf("%3d", a);
+			a++;
+        }
+        printf("\n");
+    }
+	return 0;
+}
